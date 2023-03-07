ed8501840abb8b8a972882c88f8886bd6ba6763d
Mateusz Krajewski <mateusz.krajewski@programisci.eu>
2023-03-07 20:54:16 +0100

    Initial commit

---
+++ src/test/java/com/mastior/scmtest/ScmTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mastior.scmtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScmTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
